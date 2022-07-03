e35b1da1f159d5b9d8b12ef00a54a79a46599258
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-07-03 17:39:58 +0800

    1

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Str;
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
 return [
 
     /*
