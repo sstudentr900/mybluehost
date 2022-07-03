e7c10e34d7d80ef538f040c06deb961ea8432832
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-07-03 20:15:51 +0800

    1

--- app/Models/Carousel.php
+++ app/Models/Carousel.php
@@ -1,19 +1,21 @@
-<?php
-
+<?PHP
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Carousel extends Model
-{
-    use HasFactory;
-    protected $table="carousel";
-    protected $promaryKey='id';
-    protected $fillable=[
+class Carousel extends Model {
+    //資料表名稱
+    protected $table = 'carousel';
+
+    //主鍵名稱
+    protected $promaryKey = 'id';
+
+    //可變動欄位
+    protected $fillable = [
         'sort',
         'title',
         'cover',
         'release',
     ];
 }
+?>
