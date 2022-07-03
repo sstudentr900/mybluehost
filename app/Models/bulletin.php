78e4a3043f17b7ca7964d815029297a78c5e1014
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-07-03 19:58:58 +0800

    1

--- app/Models/bulletin.php
+++ app/Models/bulletin.php
@@ -2,15 +2,12 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Bulletin extends Model {
     use HasFactory;
     //資料表名稱
-    protected $table = 'bulletin';
-
+    protected $table="bulletin";
     //主鍵名稱
     protected $promaryKey = 'id';
-
     //可變動欄位
     protected $fillable = [
         'sort',
