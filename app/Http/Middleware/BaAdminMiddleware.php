c91ef7aea9bd743c879081658cee85afe43988cc
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-07-07 11:18:52 +0800

    1

--- app/Http/Middleware/BaAdminMiddleware.php
+++ app/Http/Middleware/BaAdminMiddleware.php
@@ -32,6 +32,10 @@ class BaAdminMiddleware
             return redirect()->to('/');
         }
 
-        return $next($request);
+        // return $next($request);
+        $response = $next($request);
+        return $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma','no-cache')
+            ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
     }
 }
