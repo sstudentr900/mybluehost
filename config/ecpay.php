451a23baba55810123d05115485d14deb119b853
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2022-07-15 11:21:06 +0800

    1

---
+++ config/ecpay.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'MerchantId' =>  '2000132',//測試
+    'HashKey' =>'5294y06JbISpM5x9',//測試
+    'HashIV' =>'v77hoKGq4kWxNNIS',//測試
+    'Active' =>'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5',//測試
+    'Returnurl' =>'https://d937-211-20-170-212.jp.ngrok.io/fnmember_payment_result',//測試
+    'Orderresulturl' =>'https://d937-211-20-170-212.jp.ngrok.io/fnmember_payment_ok',//測試
+    // 'MerchantId' => '3259976', //正式
+    // 'HashKey' =>'Y3Wz1x8SPaxU0qxw', //正式
+    // 'HashIV' =>'J377elbE9zJPeFla', //正式
+    // 'Active' =>'https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5', //正式
+    // 'Returnurl' =>'http://www.finance.org.tw/fnmember_payment_result', //正式
+    // 'Orderresulturl' =>'http://www.finance.org.tw/fnmember_payment_ok', //正式
+];
