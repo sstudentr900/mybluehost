c71e096a4ace45efba7e8561ba7854330aa724ff
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2023-02-21 16:33:41 +0800

    1

--- resources/views/fnhome.blade.php
+++ resources/views/fnhome.blade.php
@@ -56,7 +56,7 @@
                         <p><span>{{ str_replace('-','/',explode(' ',$data->updated_at)[0]) }}</span></p>
                     </li>
                     @endforeach
-                     <!-- <li>
+                    <!-- <li>
                         <a href="fnnew.html">2021臺灣財務金融專題研討會－公司理財</a>
                         <p><span>2021/12/03</span></p>
                     </li>
@@ -117,7 +117,7 @@
                 </ul>
             </div>
             <div class="fbbulletin">
-                <div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/finance.org.tw/" data-tabs="timeline,events,messages" data-width="370" data-height="450" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=1059462880837642&amp;container_width=340&amp;height=450&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Ffinance.org.tw%2F&amp;locale=zh_TW&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline%2Cevents%2Cmessages&amp;width=370"><span style="vertical-align: bottom; width: 370px; height: 450px;"><iframe name="f8666def61a328" width="370px" height="450px" data-testid="fb:page Facebook Social Plugin" title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v12.0/plugins/page.php?adapt_container_width=true&amp;app_id=1059462880837642&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df7f56b4e94d48c%26domain%3D10.1.6.249%26is_canvas%3Dfalse%26origin%3Dfile%253A%252F%252F%252Ff3823c8dff47094%26relation%3Dparent.parent&amp;container_width=340&amp;height=450&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2Ffinance.org.tw%2F&amp;locale=zh_TW&amp;sdk=joey&amp;show_facepile=true&amp;small_header=false&amp;tabs=timeline%2Cevents%2Cmessages&amp;width=370" style="border: none; visibility: visible; width: 370px; height: 450px;" class=""></iframe></span></div>
+              <img src="{{ URL::asset('img').'/baslideView1669961800.jpg' }}" alt="">
             </div>
         </div>
         <div class="advertise">
